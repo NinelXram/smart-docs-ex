0e0c58b1b0f9ffb5d8581eb64fb60a9f54f37349
NinelXram <esdridz@gmail.com>
2026-03-24 12:20:45 +0700

    fix: exclude .worktrees from Vitest test discovery

--- vite.config.js
+++ vite.config.js
@@ -18,6 +18,7 @@ export default defineConfig({
     environment: 'jsdom',
     globals: true,
     setupFiles: ['./src/test/setup.js'],
+    exclude: ['**/node_modules/**', '**/.worktrees/**', '**/dist/**'],
   },
 })
 
